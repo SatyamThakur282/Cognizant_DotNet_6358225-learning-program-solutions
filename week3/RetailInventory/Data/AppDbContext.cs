570c074914e5374470341fb25dcbd61ff682fc1f
SatyamThakur282 <2230282@kiit.ac.in>
2025-07-06 22:49:51 +0530

    minor changes in name

---
+++ week3/RetailInventory/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailInventory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailInventory.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Retail_Inventory_DB;Trusted_Connection=True;");
+        }
+    }
+}
